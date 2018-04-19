99e034f9a683239b8e42afe66b2f59d3b1b03662
Marco <macatmail@gmail.com>
2018-04-19 08:50:49 +0200

    Add new maintenance/type types.

--- isCommercialSameStruct_validator.go
+++ isCommercialSameStruct_validator.go
@@ -4,12 +4,12 @@ import (
 	validator "gopkg.in/go-playground/validator.v9"
 )
 
-// isCommercialSameStruct return false if the Passed Field (param) is not "commercial".
+// isCommercialSameStruct return false if the Passed Field (param) is not "contract".
 func isCommercialSameStruct(fl validator.FieldLevel) bool {
 	field := fl.Field()
 	param := fl.Param()
 
-	if fl.Parent().FieldByName(param).String() == "commercial" {
+	if fl.Parent().FieldByName(param).String() == "contract" {
 		// field "param" is not empty
 		if field.String() == "" {
 			// current field is empty. Should not be empty!
--- maintenance_validator.go
+++ maintenance_validator.go
@@ -5,7 +5,11 @@ import (
 )
 
 // Validate maintenance type.
-// There are three types of maintenance: commercial, community or none.
+// There are three types of maintenance: internal, contract, community or none:
+// 	internal: maintenance is performed by the authors
+// 	contract: maintenance is performed by a contracted party
+// 	community: maintenance is done by the community
+// 	none: this project is unmaintained, if you file a pull request you'll likely get no response
 func maintenanceValidator(fl validator.FieldLevel) bool {
 	maintenance := fl.Field().String()
 
@@ -18,7 +22,8 @@ func maintenanceValidator(fl validator.FieldLevel) bool {
 }
 
 var maintenanceTypes = []string{
-	"commercial",
+	"internal",
+	"contract",
 	"community",
 	"none",
 }
